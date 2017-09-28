77d3f3e4cde92318f3fa78353eae9df2bb47d51f
Christopher Dimoulis <christopherjamesd@gmail.com>
2017-09-28 12:05:54 -0500

    Using columnify to print routes #3

--- src/router.js
+++ src/router.js
@@ -1,4 +1,5 @@
 const match = require('minimatch');
+const columnify = require('columnify');
 
 const Router = function(opts) {
   // Currently no options are used.
@@ -77,13 +78,15 @@ const Router = function(opts) {
   // Get a pretty listing of the routes
   this.toString = () => {
     let str = '';
+    let data = {};
     for (let i = 0; i < _routes.length; i++) {
       let route = _routes[i];
       let action = _actions[i].name || _actions[i].toString();
       str += `${route}\t\t${action}\n`;
+      data[route] = action;
     }
-
-    return str;
+    let columns = columnify(data,{columns: ['ROUTE', 'ACTION']})
+    return columns;
   }
 
   // Get all the routes
